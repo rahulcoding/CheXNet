a54109d1d1bbb2c8447da7ae4e78e0a69a115134
rahulcoding <45851454+rahulcoding@users.noreply.github.com>
2019-12-05 16:50:37 +0530

    Add files via upload

--- script.py
+++ script.py
@@ -1,40 +1,50 @@
 import os
-listy = os.listdir('/content/')
+listy = os.listdir('images')
 total_len = len(listy)
-train_len = int(total_len*0.80)
-valid_len = int(total_len*0.10) 
+#print(total_len)
+train_len = int(total_len*0.90)
+#print(train_len)
+valid_len = int(total_len*0.05) 
 new_list = []
 for j,i in enumerate(listy):
-  new_list.append(i+'\n')
-  listy.pop(j)
-  if (j == train_len):
-    break
-print(listy)
+    new_list.append(i+'\n')
+    #listy.pop(j)
+    if (j == train_len):
+        break
+#print(listy)
 new_list[-1]= new_list[-1][:-1]
-print(new_list)
+#print(len(new_list),"<-- length of train list")
 data = "".join(new_list)
-print(data)
+#print(data)
 with open('train_images.txt','w') as f:
-  f.write(data)
+    f.write(data)
 
 
 #validation
 valid_list = []
+bo = False
 for j,i in enumerate(listy):
-  valid_list.append(i+'\n')
-  listy.pop(j)
-  if (j == valid_len):
-    break
+    if (j == train_len  or bo):
+        bo = True
+        valid_list.append(i+'\n')
+        #listy.pop(j)
+        if (j == (valid_len+train_len)):
+            break
 valid_list[-1] = valid_list[-1][:-1]
+#print(len(valid_list))
 valid_data = "".join(valid_list)
 with open('valid_images.txt','w') as f:
-  f.write(valid_data)
+    f.write(valid_data)
 
 #test
 test_list = []
-for i in listy:
-  test_list.append(i+'\n')
+po = False
+for j,i in enumerate(listy):
+    if (po or j == (valid_len+train_len)):
+        po = True
+        test_list.append(i+'\n')
+#print(len(test_list))
 test_list[-1] =  test_list[-1][:-1]
 test_data = "".join(test_list)
 with open('test_images.txt','w') as f:
-  f.write(test_data)
+    f.write(test_data)
